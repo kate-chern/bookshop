637abb57173854c726c8787c0f6ae1ab4361fb01
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-09-06 06:43:19 +0300

    called methods save and findAll

---
+++ src/main/java/application/bookshop/service/BookServiceImpl.java
@@ -0,0 +1,23 @@
+package application.bookshop.service;
+
+import application.bookshop.model.Book;
+import application.bookshop.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List findAll() {
+        return bookRepository.findAll();
+    }
+}
