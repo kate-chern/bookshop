deb201c923e423715df024887255490306c0c97f
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-09-20 10:24:31 +0300

    added classes that check passwords

---
+++ src/main/java/application/bookshop/validation/FieldMatch.java
@@ -0,0 +1,24 @@
+package application.bookshop.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FieldMatchValidator.class)
+public @interface FieldMatch {
+    String message() default "{constraints.fieldmatch}";
+    String field();
+    String fieldMatch();
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        FieldMatch[] value();
+    }
+}
---
+++ src/main/java/application/bookshop/validation/FieldMatchValidator.java
@@ -0,0 +1,27 @@
+package application.bookshop.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
+    private String field;
+    private String fieldMatch;
+
+    @Override
+    public void initialize(FieldMatch constraintAnnotation) {
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
+    }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        Object fieldValue = new BeanWrapperImpl(o)
+                .getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(o)
+                .getPropertyValue(fieldMatch);
+
+        return Objects.equals(fieldValue, fieldMatchValue);
+    }
+}
