b20e20cea6ee6320a1008c05d581c6923cee2d83
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-09-20 10:29:25 +0300

    added roles for authentication

--- src/main/java/application/bookshop/controller/BookController.java
+++ src/main/java/application/bookshop/controller/BookController.java
@@ -1,16 +1,17 @@
 package application.bookshop.controller;
 
-import application.bookshop.dto.BookDto;
-import application.bookshop.dto.BookSearchParametersDto;
-import application.bookshop.dto.CreateBookRequestDto;
-import application.bookshop.service.BookService;
+import application.bookshop.dto.bookdto.BookDto;
+import application.bookshop.dto.bookdto.BookSearchParametersDto;
+import application.bookshop.dto.bookdto.CreateBookRequestDto;
+import application.bookshop.service.book.BookService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,31 +22,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Book management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/books")
 public class BookController {
     private final BookService bookService;
 
+    @PreAuthorize("hashRole('ROLE_USER')")
     @GetMapping
     @Operation(summary = "Get all books", description = "Get a list of all available books")
     public List<BookDto> findAll(Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
+    @PreAuthorize("hashRole('ROLE_USER')")
     @GetMapping("/{id}")
     @Operation(summary = "Get book by id", description = "Get one book by id")
     public BookDto findById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
+    @PreAuthorize("hashRole('ROLE_ADMIN')")
     @PostMapping
     @Operation(summary = "Create a new book", description = "Create a new book")
-    public BookDto save(@RequestBody @Valid CreateBookRequestDto requestDto) {
+    public BookDto save(Authentication authentication,
+                        @RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
+    @PreAuthorize("hashRole('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete a book by id", description = "Delete a book by id")
@@ -53,6 +58,7 @@ public class BookController {
         bookService.deleteById(id);
     }
 
+    @PreAuthorize("hashRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     @Operation(summary = "Update a book by id", description = "Update a book by id")
     public BookDto update(@PathVariable Long id,
